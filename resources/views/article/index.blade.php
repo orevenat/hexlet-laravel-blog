7629215c0f3690fa4589fde42a891e9db1f3bd08
Ruslan Knyazev <agrry@yandex.ru>
2019-12-27 21:04:32 +0500

    add articles create

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h1>Список статей</h1>
+    @if (Session::has('status'))
+        {{ Session::get('status') }}
+    @endif
     @foreach ($articles as $article)
         <h2>
             <a href="{{ route('articles.show', $article) }}">
